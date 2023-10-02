b57b8b641dfc4156d3d8d4797fe15c32a9e83129
Yazid Bougrine <yazidbougrine99@gmail.com>
2023-10-02 23:34:11 +0100

    extracted smtp credentials into their own variables

--- Infrastructure/Services/EmailSender.cs
+++ Infrastructure/Services/EmailSender.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using GestionAnnonce.Application.Common.Interfaces;
+using GestionAnnonce.Application.Exceptions;
 using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.Services
@@ -21,16 +22,28 @@ namespace Infrastructure.Services
 
         public Task SendEmailAsync(string toEmail, string subject, string body)
         {
-            var smtpSettings = _configuration.GetSection("SmtpSettings");
-            var smtpClient = new SmtpClient(smtpSettings["SmtpServer"], 587)
+            try
             {
-                EnableSsl = true,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(smtpSettings["Username"], smtpSettings["Password"])
-            };
+                var smtpSettings = _configuration.GetSection("SmtpSettings");
 
-            return smtpClient.SendMailAsync(new MailMessage(from: smtpSettings["Username"] ?? string.Empty, to: toEmail, subject,
-                body));
+                var smtpServer = smtpSettings["SmtpServer"];
+                var smtpUsername = smtpSettings["Username"];
+                var smtpPassword = smtpSettings["Password"];
+
+                var smtpClient = new SmtpClient(smtpServer, 587)
+                {
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(smtpUsername, smtpPassword)
+                };
+
+                return smtpClient.SendMailAsync(new MailMessage(from: smtpUsername ?? string.Empty, to: toEmail, subject, body));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw new EmailNotSentException("verification");
+            }
         }
     }
 }
